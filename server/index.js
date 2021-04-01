cdfe76f1a6b485984e2bd6bc69dc4a39e3a9275f
Nelson Vilte <nelsonvilte@gmail.com>
2021-03-31 23:27:31 -0300

    se actualizó configuracion  conexion

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "2012.20",
+  password: "password", //colocar el password de la base de datos
   database: "movimientos",
 });
 //rutas
